940bdad4b60e16f4cc49ce271d9355ebed0e2fa9
Ayberk Koc <ayberk@live.at>
2025-07-07 16:01:38 +0200

    imported design tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-red-800 h-screen w-full">
+        <h1 className="border-2 font-2xl-regular">
+          Whereas recognition of the inherent dignity
+        </h1>
+      </div>
+    </>
+  );
 }
