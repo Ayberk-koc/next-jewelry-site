5c3869edf881f3771ce2878bc7b553bb8888e392
Ayberk Koc <ayberk@live.at>
2025-07-08 16:28:17 +0200

    finished button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <div className="bg-red-800 h-screen w-full">
-        <h1 className="border-2 font-2xl-regular">
-          Whereas recognition of the inherent dignity
-        </h1>
+      <div className="bg-slate-800 h-screen w-full flex items-center justify-center">
+        <Button showLeadingIcon showTailingIcon>
+          Button
+        </Button>
       </div>
     </>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,55 +4,368 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-      },
+const userIconsDict = {
+  xs2: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <ellipse
+        cx="6.99935"
+        cy="10.2084"
+        rx="4.08333"
+        ry="2.04167"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="6.99935"
+        cy="4.08333"
+        r="2.33333"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xs: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <ellipse
+        cx="8.00065"
+        cy="11.6666"
+        rx="4.66667"
+        ry="2.33333"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="8.00065"
+        cy="4.66667"
+        r="2.66667"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  sm: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <ellipse
+        cx="9"
+        cy="13.125"
+        rx="5.25"
+        ry="2.625"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="9"
+        cy="5.25"
+        r="3"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  md: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <ellipse
+        cx="9.99935"
+        cy="14.5834"
+        rx="5.83333"
+        ry="2.91667"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+      <ellipse
+        cx="9.99935"
+        cy="5.83333"
+        rx="3.33333"
+        ry="3.33333"
+        stroke="currentColor"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  lg: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <ellipse
+        cx="12"
+        cy="17.5"
+        rx="7"
+        ry="3.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="12"
+        cy="7"
+        r="4"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xl: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <ellipse
+        cx="12"
+        cy="17.5"
+        rx="7"
+        ry="3.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="12"
+        cy="7"
+        r="4"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xl2: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <ellipse
+        cx="12"
+        cy="18"
+        rx="7"
+        ry="3.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="12"
+        cy="7.5"
+        r="4"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+};
+
+const tailIconsDict = {
+  xs2: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <path
+        d="M5.83398 9.91669L8.16732 7.00002L5.83398 4.08335"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xs: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M6.66602 11.3333L9.33268 7.99998L6.66602 4.66665"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  sm: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <path
+        d="M7.5 12.75L10.5 9L7.5 5.25"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  md: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M8.33398 14.1667L11.6673 10L8.33398 5.83335"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  lg: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M10 17L14 12L10 7"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xl: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M10 17L14 12L10 7"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  xl2: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+    >
+      <path
+        d="M10 17.5L14 12.5L10 7.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+};
+
+const default_size = "md";
+
+const buttonVariants = cva("inline-flex items-center justify-center", {
+  variants: {
+    variant: {
+      fill: "bg-gray-950 text-white disabled:bg-gray-200 disabled:text-gray-400",
+      outline:
+        "border border-gray-950 text-gray-950 disabled:border-gray-200 disabled:text-gray-300",
+      ghost: "text-gray-950 disabled:text-gray-300",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      xs2: "gap-gap-3 py-gap-3 px-gap-5 font-text-xs-regular",
+      xs: "gap-gap-3 py-gap-4 px-gap-5 font-text-xs-regular",
+      sm: "gap-gap-3 py-gap-4 px-gap-7 font-text-sm-regular",
+      md: "gap-gap-5 py-gap-6 px-gap-9 font-text-sm-regular",
+      lg: "gap-gap-5 py-gap-7 px-gap-10 font-text-md-regular",
+      xl: "gap-gap-6 py-gap-9 px-gap-11 font-text-md-regular",
+      xl2: "gap-gap-7 py-[18px] px-gap-12 font-text-lg-regular",
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "fill",
+    size: default_size,
+  },
+});
 
 function Button({
   className,
   variant,
   size,
+  iconOnly = false,
+  showLeadingIcon = false,
+  showTailingIcon = false,
   asChild = false,
+  children,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    iconOnly?: boolean;
+    showLeadingIcon?: boolean;
+    showTailingIcon?: boolean;
   }) {
   const Comp = asChild ? Slot : "button";
+
+  if (!size) {
+    size = default_size;
+  }
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      {(showLeadingIcon || iconOnly) && userIconsDict[size]}
+      {!iconOnly && <span className="mb-[3px]">{children}</span>}
+      {showTailingIcon && tailIconsDict[size]}
+    </Comp>
   );
 }
 
